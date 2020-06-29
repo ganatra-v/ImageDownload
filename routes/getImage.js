b03ad46449addf45d5c35774d97a747627e12f5b
Vaibhav Ganatra <vaib2641@gmail.com>
2020-06-29 15:12:31 +0530

    Added pagination to get apis

--- routes/getImage.js
+++ routes/getImage.js
@@ -4,20 +4,47 @@ const router = express.Router();
 const Image = require("../models/image");
 
 async function getImages(req,res){
+   let images=[];
+   let {page=1,limit=2}= req.query;
+   let count=0;
    try{
       if(req.headers.name)
       {
          const name= req.headers.name;
-         const images= await Image.find({name:name})
-            .select("-__v");
-         res.status(200).json(images);
+         images= await Image.find({name:name})
+            .select("-__v")
+            .limit(limit*1)
+            .skip((page-1)*limit)
+            .exec();
+         await Image.countDocuments({name:name},(err,c)=>{
+            if(err)
+               console.log(err);
+            else
+               count= c;
+         });
       }
       else{
-         const images= await Image.find().select("-__v");
-         res.status(200).json(images);
+         images= await Image.find()
+            .select("-__v")
+            .limit(limit*1)
+            .skip((page-1)*limit)
+            .exec();
+         await Image.countDocuments({},(err,c)=>{
+         if(err)
+            console.log(err);
+         else
+            count=c;
+      });
       }
+      if(count==0) page=0;
+      res.status(200).json({
+         currentPage:page,
+         totalPages:Math.ceil(count/limit),
+         images:images
+      });
    }
    catch(err){
+      console.log(err);
       res.status(500).json({
          success:false,
          error:err
